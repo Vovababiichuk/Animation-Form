af88ecd9ecfc4f7be2f1900e9e8de735a54a2f92
Vovababiichuk <babihuk888gmail.com>
2023-06-03 10:26:35 +0300

    FIX img + delet print text

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,33 @@
 const mainDiv = document.getElementById("bg");
-const backgrounds = ["background-1", "background-2", "background-3", "background-4", "background-5"];
+const backgrounds =
+[
+  "background-1",
+  "background-2",
+  "background-3",
+  "background-4",
+  "background-5",
+  "background-6",
+  "background-7",
+  "background-8",
+  "background-9",
+  "background-10",
+  "background-11",
+  "background-12",
+  "background-13",
+  "background-14",
+  "background-15",
+  "background-16",
+  "background-17",
+  "background-18",
+  "background-19",
+  "background-20",
+  "background-21",
+  "background-22",
+  "background-23",
+  "background-24",
+  "background-25",
+  "background-26",
+];
 let currentIndex = -1;
 
 function toggleBackground() {
@@ -25,30 +53,30 @@ function toggleBackground() {
 
 
 
-setTimeout(function() {
-  animateLines();
-}, 3000);
+// setTimeout(function() {
+//   animateLines();
+// }, 3000);
 
-function resetAnimation(element) {
-  element.style.animation = 'none';
-  element.offsetHeight;
-  element.style.animation = null;
-}
+// function resetAnimation(element) {
+//   element.style.animation = 'none';
+//   element.offsetHeight;
+//   element.style.animation = null;
+// }
 
-function animateLines() {
-  const lines = document.querySelectorAll('.line');
-  const delayBetweenLines = 3000;
+// function animateLines() {
+//   const lines = document.querySelectorAll('.line');
+//   const delayBetweenLines = 3000;
 
-  function animateLine(line, index) {
-    line.style.animation = 'animated-text 3s steps(30, end) forwards';
-    line.addEventListener('animationend', function() {
-      resetAnimation(line);
-      animateLine(lines[(index + 1) % lines.length], (index + 1) % lines.length);
-    });
-  }
+//   function animateLine(line, index) {
+//     line.style.animation = 'animated-text 3s steps(30, end) forwards';
+//     line.addEventListener('animationend', function() {
+//       resetAnimation(line);
+//       animateLine(lines[(index + 1) % lines.length], (index + 1) % lines.length);
+//     });
+//   }
 
-  animateLine(lines[0], 0);
-}
+//   animateLine(lines[0], 0);
+// }
 
 
 
